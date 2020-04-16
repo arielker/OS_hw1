798184530d5ad00a9c7a4f9c53908bc4bed59c8e
vYahav <51291115+vYahav@users.noreply.github.com>
2020-04-16 21:22:19 +0300

    ctrl z and c done

--- signals.cpp
+++ signals.cpp
@@ -12,9 +12,6 @@ void ctrlCHandler(int sig_num) {
 	SmallShell& smash = SmallShell::getInstance();
 	std::cout<<"smash: got ctrl-C"<<std::endl;
 	pid_t p = smash.getCurrentFgPid();
-	//int foreground_pid = getpid(); //TODO: change this?
-	//std::cout<<"smash: process "<<foreground_pid<<" was killed"<<std::endl;
-	//cout << "p: "<< p << " smash: " << smash.getSmashPid() <<endl;
 	if(p != smash.getSmashPid()){
 		if(kill(p, SIGKILL) == -1){
 			perror("smash error: kill failed");
@@ -23,16 +20,13 @@ void ctrlCHandler(int sig_num) {
 			smash.setCurrentFgPid(smash.getSmashPid());
 		}
 	}
-	//kill(foreground_pid,SIGKILL); 
+	
 }
 
 void ctrlZHandler(int sig_num) {
 	SmallShell& smash = SmallShell::getInstance();
 	std::cout<<"smash: got ctrl-Z"<<std::endl;
 	pid_t p = smash.getCurrentFgPid();
-	//TODO: add the foreground process to jobs list. 
-	//If no process is running in the foreground,
-	//then nothing will be added to jobs list.
 	if(p != smash.getSmashPid()){
 		if(kill(p, SIGSTOP) == -1){
 			perror("smash error: kill failed");
@@ -42,9 +36,6 @@ void ctrlZHandler(int sig_num) {
 			smash.setCurrentFgPid(smash.getSmashPid());
 		}
 	}
-	//int foreground_pid=getpid();//TODO: change this?
-	//std::cout<<"smash: process "<<foreground_pid<<" was stopped"<<std::endl;
-	//kill(foreground_pid,SIGSTOP); 
 }
 
 void alarmHandler(int sig_num) {
@@ -53,5 +44,5 @@ void alarmHandler(int sig_num) {
   //and set pid to the command's pid.
   int pid=-99;//TODO: change this! (-99 to real pid)
   kill(pid,SIGKILL);
-  
+  //cout<<"smash: "<<[command-line]<<" timed out!"<<endl;
 }
